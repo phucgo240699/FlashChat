b7c286b330cb781776d27b2c84ccfa0c1c2565cd
phucgo240699 <phucgo240699@gmail.com>
2019-11-22 11:37:00 +0700

    Use CLTypingLabel to make animation for title in welcome view controller

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -7,28 +7,28 @@
 //
 
 import UIKit
-
+import CLTypingLabel
 class WelcomeViewController: UIViewController {
 
-    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var titleLabel: CLTypingLabel!
     
-    func AnimationTitle(){
-        titleLabel.text=""
-        let titleText="⚡️FlashChat"
-        var charIndex=0.0
-        for letter in titleText{
-            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { (timer) in
-                self.titleLabel.text?.append(letter)
-            }
-            charIndex+=1
-            
-        }
-    }
+//    func AnimationTitle(){
+//        titleLabel.text=""
+//        let titleText="⚡️FlashChat"
+//        var charIndex=0.0
+//        for letter in titleText{
+//            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { (timer) in
+//                self.titleLabel.text?.append(letter)
+//            }
+//            charIndex+=1
+//
+//        }
+//    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        AnimationTitle()
+        titleLabel.text="⚡️FlashChat"
        
     }
     
