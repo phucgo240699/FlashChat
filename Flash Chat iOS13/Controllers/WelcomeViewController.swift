40ca3e02032e7a6e3d3480d5fa217898b6190791
phucgo240699 <phucgo240699@gmail.com>
2019-11-22 10:41:21 +0700

    Add segways, add animation for title

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -12,9 +12,22 @@ class WelcomeViewController: UIViewController {
 
     @IBOutlet weak var titleLabel: UILabel!
     
+    func AnimationTitle(){
+        let titleText="⚡️FlashChat"
+        var charIndex=0.0
+        for letter in titleText{
+            Timer.scheduledTimer(withTimeInterval: 0.1*charIndex, repeats: false) { (timer) in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex+=1
+            
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        AnimationTitle()
        
     }
     
